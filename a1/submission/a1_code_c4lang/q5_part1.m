0de28a2bde2ef65cd7d57dd9f39760dd18da4825
Lang Chen <marklang1993@gmail.com>
2017-06-05 16:59:06 -0400

    a1: Add files for “Learn” submission.

---
+++ a1/submission/a1_code_c4lang/q5_part1.m
@@ -0,0 +1,13 @@
+clear;
+mdata=load('data.mat');
+
+hold on;
+title('Q5 Part1 - Data Points with * Symbol');
+xlabel('X axis'); % x-axis label
+ylabel('Y axis'); % y-axis label
+
+% axes('position', [0.1, 0.1, 0.8, 0.8]); % setup the axes
+% axis equal; % Tip: use axis equal to avoid scaling issues
+
+plot(mdata.x, mdata.y, '*');
+grid on;
---
+++ a1/submission/a1_code_c4lang/q5_part3_1st.m
@@ -0,0 +1,20 @@
+clear;
+mdata=load('data.mat');
+end_cond = 'variational'; % 1st Figure
+
+% plot heart
+[px1, py1] = splinet(mdata.x(1:55), mdata.y(1:55), end_cond);
+% plot 'L'
+[px2, py2] = splinet(mdata.x(56:67), mdata.y(56:67), end_cond); 
+% plot 'C'
+[px3, py3] = splinet(mdata.x(68:83), mdata.y(68:83), end_cond);
+% plot ampersand
+[px4, py4] = splinet(mdata.x(84:105), mdata.y(84:105), end_cond);
+
+hold on;
+plot(px1, py1, 'b');
+plot(px2, py2, 'b');
+plot(px3, py3, 'b');
+plot(px4, py4, 'b');
+title('Q5 Part3 - Spline Interpolation - Natural');
+axis off;
