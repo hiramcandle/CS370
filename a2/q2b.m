c60b601051be01f677ec44453a894dfed4263c80
marklang1993 <marklang1993@gmail.com>
2017-06-08 17:56:06 -0400

    a2: Correct q2b and q2c.

--- a2/q2b.m
+++ a2/q2b.m
@@ -9,11 +9,16 @@ y_0 = 1;
 h = (t_f - t_0) / N;
 
 % calculate Y
-y = ForwardEuler(@q2func, t_0, t_f, N, y_0);
 t = t_0:h:t_f;
+len = length(t);
+y = zeros(len);
+for i = 1 : len
+   y(i) = q2func_exact(t(i));
+end
+y_n = ForwardEuler(@q2func, t_0, t_f, N, y_0);
 
 % plot
 hold on;
 plot(t, y);
-plot(t, y, '.');
+plot(t, y_n, '.');
 title('Q2(b) ForwardEuler');
